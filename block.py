3dd03527bd9e7d349684c24308c2ab7e04a28fd6
Leo Edwards <thejpe100@gmail.com>
2017-10-25 13:25:52 +0100

    v0.1

--- block.py
+++ block.py
@@ -57,7 +57,7 @@ def create_first_block():
     block_data['index'] = 0
     block_data['timestamp'] = date.datetime.now()
     block_data['data'] = 'First block data'
-    block_data['prev_hash'] = None
+    block_data['prev_hash'] = 'None'
     block = Block(block_data)
     return block
 
